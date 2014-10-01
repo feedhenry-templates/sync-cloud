a91e96026719f575d2789c30dcf01da24b8b53a7
David Martin <david.martin@feedhenry.com>
2014-10-01 14:53:03 +0100

    fhlint fixes

--- application.js
+++ application.js
@@ -1,14 +1,18 @@
-var express = require('express');
 var mbaasApi = require('fh-mbaas-api');
+var express = require('express');
 var mbaasExpress = mbaasApi.mbaasExpress();
+var cors = require('cors');
 
-// Define custom sync handlers and interceptors
-require('./lib/sync.js');
-
-// Securable endpoints: list the endpoints which you want to make securable here
-var securableEndpoints = [];
+// list the endpoints which you want to make securable here
+var securableEndpoints;
+// fhlint-begin: securable-endpoints
+securableEndpoints = [];
+// fhlint-end
 
 var app = express();
+
+// Enable CORS for all requests
+app.use(cors());
 
 // Note: the order which we add middleware to Express here is important!
 app.use('/sys', mbaasExpress.sys(securableEndpoints));
@@ -17,12 +21,18 @@ app.use('/mbaas', mbaasExpress.mbaas);
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
-// Add extra routes here
+// allow serving of static files from the public directory
+app.use(express.static(__dirname + '/public'));
+
+// fhlint-begin: custom-routes
+// Define custom sync handlers and interceptors
+require('./lib/sync.js');
+// fhlint-end
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001;
-var server = app.listen(port, function(){
+var server = app.listen(port, function() {
   console.log("App started at: " + new Date() + " on port: " + port);
 });
