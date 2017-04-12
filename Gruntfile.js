724f2cec5f7acc5392bc00089a496e212be7cc97
Corinne Krych <corinnekrych@gmail.com>
2017-04-12 16:36:07 +0200

    :recycle: clean unused build targets

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,61 +47,11 @@ module.exports = function(grunt) {
       local: {
         FH_USE_LOCAL_DB: true
       }
-    },
-    shell: {
-      unit: {
-        options: {
-          stdout: true,
-          stderr: true
-        },
-        command: 'env NODE_PATH=. ./node_modules/.bin/turbo test/unit'
-      },
-      accept: {
-        options: {
-          stdout: true,
-          stderr: true
-        },
-        command: 'env NODE_PATH=. ./node_modules/.bin/turbo --setUp=test/accept/server.js --tearDown=test/accept/server.js test/accept'
-      },
-      coverage_unit: {
-        options: {
-          stdout: true,
-          stderr: true
-        },
-        command: [
-          'rm -rf coverage cov-unit',
-          'env NODE_PATH=. ./node_modules/.bin/istanbul cover --dir cov-unit ./node_modules/.bin/turbo -- test/unit',
-          './node_modules/.bin/istanbul report',
-          'echo "See html coverage at: `pwd`/coverage/lcov-report/index.html"'
-        ].join('&&')
-      },
-      coverage_accept: {
-        options: {
-          stdout: true,
-          stderr: true
-        },
-        command: [
-          'rm -rf coverage cov-accept',
-          'env NODE_PATH=. ./node_modules/.bin/istanbul cover --dir cov-accept ./node_modules/.bin/turbo -- --setUp=test/accept/server.js --tearDown=test/accept/server.js test/accept',
-          './node_modules/.bin/istanbul report',
-          'echo "See html coverage at: `pwd`/coverage/lcov-report/index.html"'
-        ].join('&&')
-      }
     }
   });
 
   // Load NPM tasks
   require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
-
-  // Testing tasks
-  grunt.registerTask('test', ['shell:unit', 'shell:accept']);
-  grunt.registerTask('unit', ['shell:unit']);
-  grunt.registerTask('accept', ['env:local', 'shell:accept']);
-
-  // Coverate tasks
-  grunt.registerTask('coverage', ['shell:coverage_unit', 'shell:coverage_accept']);
-  grunt.registerTask('coverage-unit', ['shell:coverage_unit']);
-  grunt.registerTask('coverage-accept', ['env:local', 'shell:coverage_accept']);
 
   // Making grunt default to force in order not to break the project.
   grunt.option('force', true);
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FeedHenry Data Sync Tutorial Cloud App
 [![Dependency Status](https://img.shields.io/david/feedhenry-templates/sync-cloud.svg?style=flat-square)](https://david-dm.org/feedhenry-templates/sync-cloud)
+
+# Build
+```
+npm install
+```
